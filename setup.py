59283f0a928c7f6f2a8a707b767913fc23a66092
Flavio Curella <flavio.curella@gmail.com>
2015-10-01 13:16:47 -0500

    Add py3.3 trove classifier

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
     install_requires=[
