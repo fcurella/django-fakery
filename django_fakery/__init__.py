03f08e09bf178eb5a81eb9324ef9dfcc0841d009
Flavio Curella <flavio.curella@gmail.com>
2016-01-07 17:07:18 -0600

    Bump version: 1.2.3 → 1.2.4

--- django_fakery/__init__.py
+++ django_fakery/__init__.py
@@ -1,4 +1,4 @@
-VERSION = '1.2.3'
+VERSION = '1.2.4'
 
 from django.utils.functional import SimpleLazyObject
 from django.utils.module_loading import import_string
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages, setup
 
 
-VERSION = '1.2.3'
+VERSION = '1.2.4'
 
 
 def read(fname):
