7dbf8dea2b02f828b5ea8229ead044fd1e7c627b
Flavio Curella <flavio.curella@gmail.com>
2017-06-09 16:30:55 -0500

    fix timezone issue

--- django_fakery/tests/test_shortcuts.py
+++ django_fakery/tests/test_shortcuts.py
@@ -13,11 +13,11 @@ class ShortcutsTest(TestCase):
     def test_future_datetime(self):
         fn = shortcuts.future_datetime()
         date = fn(1, fake)
-        self.assertTrue(date > timezone.make_aware(timezone.now()))
+        self.assertTrue(date > timezone.now())
 
         fn = shortcuts.future_datetime('+2d')
         date = fn(1, fake)
-        self.assertTrue(date > timezone.make_aware(timezone.now()))
+        self.assertTrue(date > timezone.now())
 
     def test_future_date(self):
         fn = shortcuts.future_date()
@@ -31,11 +31,11 @@ class ShortcutsTest(TestCase):
     def test_past_datetime(self):
         fn = shortcuts.past_datetime()
         date = fn(1, fake)
-        self.assertTrue(date < timezone.make_aware(timezone.now()))
+        self.assertTrue(date < timezone.now())
 
         fn = shortcuts.past_datetime('-2d')
         date = fn(1, fake)
-        self.assertTrue(date < timezone.make_aware(timezone.now()))
+        self.assertTrue(date < timezone.now())
 
     def test_past_date(self):
         fn = shortcuts.past_date()
