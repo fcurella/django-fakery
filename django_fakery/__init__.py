a7913683e669c7acbba6f02c79a3cd037a61c611
Flavio Curella <flavio.curella@gmail.com>
2017-12-27 16:52:44 -0600

    Bump version: 1.8.4 → 1.8.5

--- django_fakery/__init__.py
+++ django_fakery/__init__.py
@@ -1,4 +1,4 @@
-VERSION = '1.8.4'
+VERSION = '1.8.5'
 
 from django.utils.functional import SimpleLazyObject
 from django.utils.module_loading import import_string
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages, setup
 
 
-VERSION = '1.8.4'
+VERSION = '1.8.5'
 
 
 def read(fname):
