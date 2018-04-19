442c73359c51afd7c3a1c4f3a6b5356bd3d517ef
Flavio Curella <flavio.curella@gmail.com>
2018-04-19 15:41:11 -0500

    fix requirement in setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ],
     install_requires=[
         "Faker>=0.8.0,<0.9.0",
-        "Django>=1.8",
+        "Django>=1.11",
         "six>=1.10.1",
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
