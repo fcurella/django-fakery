02b0c265b9067b68cddfc31ea72f9f0bdf4872a8
Nikita Sobolev <mail@sobolevn.me>
2019-12-06 13:48:49 +0300

    Makes shortcuts a fixture
    I end up importing `shortcuts` in almost every module.
    I guess, that it would much more convenient to use:
    
    ```python
    def test_with_some_date(fakery, shortcuts):
         ...
    ```
    
    I hope this is a good idea :)

--- django_fakery/plugin.py
+++ django_fakery/plugin.py
@@ -1,8 +1,14 @@
 import pytest
 
 from django_fakery import factory
+from django_fakery import shortcuts as _shortcuts
 
 
 @pytest.fixture
 def fakery():
     return factory
+
+
+@pytest.fixture
+def shortcuts():
+    return _shortcuts
